12c4f449a73f9b965e2f17806c3ee4dc3cd4fdde
jieun kim <jieun kim>
2016-12-04 18:30:39 +0900

    address search

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,8 +1,8 @@
-script(src="http://dmaps.daum.net/map_js_init/postcode.v2.js")
-script
-    new daum.Postcode({
-        oncomplete: function(data) {
-            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-            // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+
+$(function(){
+    $(".addressSearch").click(function() {
+        if(confirm("확인?")){
+            console.log("check");
         }
-    }).open();
+    });
+});
